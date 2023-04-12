ec6ab3c8f7a946296ccc35aacb77cb9e7995a3c9
yj4165 <yeayen789@gmail.com>
2023-04-12 08:55:34 +0000

    change:file

--- utils.py
+++ utils.py
@@ -10,93 +10,91 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 class ClassifierDataset(Dataset):
-    def __init__(self, processor, df):
-        super().__init__()
-        self.context_length = 77
-        self.df = df
-        self.processor = processor
-        self.org = []
-
-        for idx in tqdm(range(len(df))):
-            org = df.iloc[idx]
-
-            org_input = self.processor(text=org.title, return_tensors="pt", padding="max_length",
-                                       max_length=self.context_length)
-            org_input['input_ids'] = torch.squeeze(org_input['input_ids'])
-            org_input['attention_mask'] = torch.squeeze(org_input['attention_mask'])
-
-            self.org.append(org_input)
-
-    def __len__(self):
-        return len(self.org)
-
-    def __getitem__(self, idx):
-        row = self.df.iloc[idx]
-        image = Image.open(f"{DataArguments.IMAGE_PATH}/{row['link'][-19:]}.jpg")
-        img_input = self.processor(images=image, return_tensors="pt", padding="max_length",
-                                   max_length=self.context_length)
-        img_input['pixel_values'] = torch.squeeze(img_input['pixel_values'])
-        label = torch.from_numpy(np.asarray(row['label']))
-        return self.org[idx], img_input, label
-
-# 모든 loss에 적용할 수 있도록 코드 수정 필요
-class ContrastiveDataset(Dataset):
-    def __init__(self, processor, df):
+    def __init__(self, processor, df, img_path = None, org_col=None):
         super().__init__()
         self.context_length = 77
         self.df = df
         self.processor = processor
         self.orgs = []
-        self.negs = []
+        self.img_path = img_path
+        self.org_col = org_col
 
         for idx in tqdm(range(len(df))):
-            row = self.df.iloc[idx] # dataFrame에 모두 넣어 두고 분할 할 것인지 각각 분할할 것인지 결정
+            org = self.df.iloc[idx]
 
-            org_input = self.processor(text=row.title, return_tensors="pt", padding="max_length",
-                                       max_length=self.context_length) # CLIP 허용 length 77로 모두 padding
-            org_input['input_ids'] = torch.squeeze(org_input['input_ids']) # processor가 붙여주는 배치 제거
+            org_input = self.processor(text=org[self.org_col], return_tensors="pt", padding="max_length",
+                                       max_length=self.context_length)
+            org_input['input_ids'] = torch.squeeze(org_input['input_ids'])
+            org_input['attention_mask'] = torch.squeeze(org_input['attention_mask'])
+
+            self.orgs.append(org_input)
+
+    def __len__(self):
+        return len(self.orgs)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        img_input = torch.load(f"{self.img_path}/{row.hash_id}.pt")
+        label = torch.from_numpy(np.asarray(row['label']))
+        return self.orgs[idx], img_input, label
+
+# 모든 loss에 적용할 수 있도록 코드 수정 필요
+class ContrastiveDataset(Dataset):
+    def __init__(self, processor, df, img_path = None, org_col=None, neg_col=None):
+        super().__init__()
+        self.context_length = 77
+        self.df = df
+        self.processor = processor
+        self.orgs = []
+        self.negs = [] 
+        self.img_path = img_path
+        self.org_col = org_col
+        self.neg_col = neg_col
+        
+        for idx in tqdm(range(len(df))):
+            row = self.df.iloc[idx] # dataFrame에 모두 넣어 두고 분할 할 것인지 각각 분할할 것인지 결정
+            o_row = row[self.org_col]
+            n_row = row[self.neg_col]
+            
+            org_input = self.processor(text=o_row, return_tensors="pt", padding="max_length",
+                                       max_length=self.context_length)
+            
+            org_input['input_ids'] = torch.squeeze(org_input['input_ids'])
             org_input['attention_mask'] = torch.squeeze(org_input['attention_mask'])
             
-            if DataArguments.NEGATIVE_TITLE == 'SHUFFLE':
-                neg_input = self.processor(text=row.HN_SHUF, return_tensors="pt", padding="max_length",
-                                           max_length=self.context_length)
-            elif DataArguments.NEGATIVE_TITLE == 'MASK_PREDICT':
-                neg_input = self.processor(text=row.HN_MASK_PREDICT, return_tensors="pt", padding="max_length",
-                                           max_length=self.context_length)
-            else:
-                print("Negative Data Load error")
-                
+            
+            neg_input = self.processor(text=n_row, return_tensors="pt", padding="max_length",
+                                       max_length=self.context_length)
+            
             neg_input['input_ids'] = torch.squeeze(neg_input['input_ids'])
             neg_input['attention_mask'] = torch.squeeze(neg_input['attention_mask'])
-                        
+            
             self.orgs.append(org_input)
             self.negs.append(neg_input)
 
     def __len__(self):
-        return len(self.df)
+        return len(self.orgs)
 
     def __getitem__(self, idx):
-        row = self.df.iloc[idx]
-        
-        image = Image.open(f"{DataArguments.IMAGE_PATH}/{row['source']}/{row['hash_id']}.png").convert('RGB') # 메모리 load 너무 커서 나눔
-        img_input = self.processor(images=image, return_tensors="pt", padding="max_length",
-                                    max_length=self.context_length)
-        img_input['pixel_values'] = torch.squeeze(img_input['pixel_values'])
-            
+        row = self.df.iat[idx,0]
+        img_input = torch.load(f"{self.img_path}/{row}.pt")
         return self.orgs[idx], self.negs[idx], img_input
 
-def create_data_loader(processor=None, df=None, batch_size=None, num_workers=None, shuffle = None, drop_last=None):
-    if ModelArguments.TRAIN_MODEL == 'contrastive':
-        cd = ContrastiveDataset(processor, df)
+
+    
+def create_data_loader(processor=None, df=None, batch_size=None, num_workers=None, shuffle = None, drop_last=None, org_col=None, neg_col=None,  pin_memory=False):
+    if ModelArguments.TRAIN_MODEL == 'contrastive': 
+        cd = ContrastiveDataset(processor, df, DataArguments.IMAGE_TENSOR, org_col, neg_col)
     else:
-        cd = ClassifierDataset(processor, df)
+        cd = ClassifierDataset(processor, df, DataArguments.IMAGE_TENSOR, org_col)
 
     return DataLoader(
         cd,
         batch_size=batch_size,
         num_workers=num_workers,
         shuffle=shuffle,
-        drop_last=drop_last
+        drop_last=drop_last,
+        pin_memory=pin_memory
     )
 
 def set_seed(seed):
@@ -136,30 +134,31 @@ def compute_accuracy(y_target, y_pred):
 def classifier_tuplify_with_device(batch, device):
     return tuple([batch[0]['input_ids'].to(device, dtype=torch.long),
                       batch[0]['attention_mask'].to(device, dtype=torch.long),
-                      batch[1]['pixel_values'].to(device, dtype=torch.float),
+                      batch[1].to(device, dtype=torch.float),
                       batch[2].to(device, dtype=torch.float)])
 
 def contrastive_tuplify_with_device(batch, device):
     if ModelArguments.Contrastive_Mode == 'imgtotxt':
         return tuple([batch[0]['input_ids'].to(device, dtype=torch.long),
                       batch[0]['attention_mask'].to(device, dtype=torch.long),
-                      batch[2]['pixel_values'].to(device, dtype=torch.float)])
+                      batch[2].to(device, dtype=torch.float)])
     elif ModelArguments.Contrastive_Mode == 'imgtotxt_H':
         return tuple([batch[0]['input_ids'].to(device, dtype=torch.long),
                       batch[0]['attention_mask'].to(device, dtype=torch.long),
                       batch[1]['input_ids'].to(device, dtype=torch.long),
                       batch[1]['attention_mask'].to(device, dtype=torch.long),
-                      batch[2]['pixel_values'].to(device, dtype=torch.float)])
+                      batch[2].to(device, dtype=torch.float)])
     elif ModelArguments.Contrastive_Mode == 'txttotxt_H':
         return tuple([batch[0]['input_ids'].to(device, dtype=torch.long),
                       batch[0]['attention_mask'].to(device, dtype=torch.long),
                       batch[1]['input_ids'].to(device, dtype=torch.long),
-                      batch[1]['attention_mask'].to(device, dtype=torch.long)])
-    elif ModelArguments.Contrastive_Mode =='imgtoimg_H_txttotxt_H':
+                      batch[1]['attention_mask'].to(device, dtype=torch.long),
+                      batch[2].to(device, dtype=torch.float)])
+    elif ModelArguments.Contrastive_Mode =='imgtotxt_txttotxt_H':
         return tuple([batch[0]['input_ids'].to(device, dtype=torch.long),
                       batch[0]['attention_mask'].to(device, dtype=torch.long),
                       batch[1]['input_ids'].to(device, dtype=torch.long),
                       batch[1]['attention_mask'].to(device, dtype=torch.long),
-                      batch[2]['pixel_values'].to(device, dtype=torch.float)])
+                      batch[2].to(device, dtype=torch.float)])
     else:
         return -1
